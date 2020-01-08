90489ebefaf593bb6ef724b6d0007a1328423dcc
Dauda Hammed <dauda.oluwatoyin.hammed@gmail.com>
2020-01-08 17:16:03 +0100

    Updates

---
+++ precache-manifest.99858ef93203958da28c00f10a318369.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fdb3d40fb2a3d35beb132761a4bd7d6",
+    "url": "/react-monster-rolodex-project/index.html"
+  },
+  {
+    "revision": "664054f9cd43b0492637",
+    "url": "/react-monster-rolodex-project/static/css/main.6ef7626f.chunk.css"
+  },
+  {
+    "revision": "8d82078e3494a7266b17",
+    "url": "/react-monster-rolodex-project/static/js/2.352aa1eb.chunk.js"
+  },
+  {
+    "revision": "664054f9cd43b0492637",
+    "url": "/react-monster-rolodex-project/static/js/main.269e7116.chunk.js"
+  },
+  {
+    "revision": "245d94bbc58c82759ef0",
+    "url": "/react-monster-rolodex-project/static/js/runtime~main.c32a0cda.js"
+  }
+]);
